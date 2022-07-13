47f44d5480a4e3620598d61c6c21952f8970f28c
mohamed maher <mohamedmaher.mm330@gmail.com>
2022-07-13 16:58:45 +0200

    add skills comp

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import Projects from "./components/Projects/Projects";
 import Project from "./components/project/Project";
 import Href from "./hrefs/Href";
 import IconMode from "./components/icon/IconMode";
-import About from "./pages/About/About"
+import About from "./pages/About/About";
 import { useState } from "react";
+import Skills from "./pages/skills/Skills";
 let classNameApp = "App dark";
 
 function App() {
@@ -32,13 +33,26 @@ function App() {
               </>
             }
           />
-          <Route path="/about" element={
-            <>
-            <Navbar/>
-            <About/>
-              <IconMode changeMode={changeMode} mode={DarkMode}/>
-            </>
-          }/>
+          <Route
+            path="/Skills"
+            element={
+              <>
+                <Navbar />
+                <Skills />
+                <IconMode changeMode={changeMode} mode={DarkMode} />
+              </>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <>
+                <Navbar />
+                <About />
+                <IconMode changeMode={changeMode} mode={DarkMode} />
+              </>
+            }
+          />
           <Route path="/project/:id" element={<Project hrefs={Href} />} />
         </Routes>
       </div>
---
+++ src/pages/skills/Skills.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Skills.css";
+const skills = [
+  { id: 1, title_skills: "HTML", pir: 80 },
+  { id: 2, title_skills: "CSS", pir: 80 },
+  { id: 3, title_skills: "JS", pir: 75 },
+  { id: 4, title_skills: "Bootstrap", pir: 95 },
+  { id: 5, title_skills: "REACT", pir: 80 },
+  { id: 6, title_skills: "JSON", pir: 100 },
+  { id: 7, title_skills: "EJS", pir: 90 },
+  { id: 8, title_skills: "GULP", pir: 90 },
+  { id: 9, title_skills: "SASS", pir: 90 },
+  { id: 10, title_skills: "jquery", pir: 50 },
+];
+function Skills() {
+  return (
+    <>
+      {/* section skills */}
+      <div className="container">
+        <h3 className="set_title">Skills</h3>
+        <div className="skills ">
+          {skills.map((skill) => {
+            return (
+              <div className="skills_box">
+                <div key={skill.id} className="skill_title">
+                  {skill.title_skills}
+                </div>
+                <div key={skill.id} className="skill_progress">
+                  <span
+                    className={`progress active`}
+                    style={{ width: skill.pir + "%" }}
+                  >
+                    {skill.pir}%
+                  </span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Skills;
